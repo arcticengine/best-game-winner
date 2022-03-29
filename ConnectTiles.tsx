50882ee1e269c2c74754aecb00fdeb6ad4e26578
pepega1223 <pepega__@outlook.com>
2022-03-29 10:15:44 +0300

    Update ConnectTiles.tsx

--- ConnectTiles.tsx
+++ ConnectTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="ConnectTiles" tilewidth="64" tileheight="64" tilecount="1024" columns="32">
- <image source="../gaijin jam/ConnectTiles.png" width="2048" height="2048"/>
+ <image source="ConnectTiles.png" width="2048" height="2048"/>
 </tileset>
